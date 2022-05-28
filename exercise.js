b3fdb9a0d6823515e341a53e747e01500c61e0a8
sazzadhossain1 <sazzadtomal1@gmail.com>
2022-05-28 19:13:08 +0600

    first commit

---
+++ exercise.js
@@ -0,0 +1,47 @@
+// const num = 1;
+// while (num <= 10) {
+//   console.log(num);
+//   num += 2;
+// }
+
+//loop er je condition deya aache sheta (num) er value jotokhon porjonto 10 er kom othoba 10 er shoman na hoy totokhon porjonto cholbe r protibar num er value 2 change kore barabe and joknon value 9 hobe tokhon theme jabe.11 hobe na karon 11 10 er chaite boro.kintu (const) use korar karone amader lppo ti cholte parbe na karon (const) er value second time change kora jay na tai amader let use kora lagbe.
+
+// let num1 = 1;
+// while (num1 <= 10) {
+//   console.log(num1);
+//   num1 += 2;
+// }
+//-----------------------------------------------------------
+
+// let num = 1;
+// while (num <= 20) {
+//   if (num % 4 === 0) {
+//     console.log(num);
+//   }
+//   num++;
+// }
+//amra ai loop er vitore jei condition diyechi,shei condition onujai num er value barbe 20er shoman porjonto othoba 20 er kom porjonto.protibar value 4 kore bartese and 20 e jeye theme jacche karon condition onujai 20 er beshi jete parbe na.same vabe amra aikhane o let use korbo.
+
+//-----------------------------------------------------------
+
+// let num = 100;
+// while (num < 150) {
+//   console.log(num + 1);
+//   num--;
+// }
+//jokhon ai loop ti ke chalabo tokhon ai loop ti choltei thakbei r (-)kortei thakbe and ata choltei thakbe.
+//-------------------------------------------------------
+
+// let num = 0;
+// while (num <= 100) {
+//   console.log(num);
+//   num += 2;
+// }
+
+// for (num = 0; num < 102; num += 2) {
+//   console.log(num);
+// }
+
+//-----------------------------------------------
+
+//
